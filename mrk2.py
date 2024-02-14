e227ce2703e9612424e73e596346e9d05a12c157
aadityapattabhiraman <154038229+aadityapattabhiraman@users.noreply.github.com>
2024-02-14 14:20:30 +0530

    modified mrk2.py

--- mrk2.py
+++ mrk2.py
@@ -22,3 +22,21 @@ t += x
 # exponential
 z = x.pow(2)
 z = x ** 2
+
+# simple comparison
+z = x < 0
+z = x > 0
+
+# matmul
+x1 = torch.rand((2, 5))
+x2 = torch.rand((5, 3))
+x3 = torch.mm(x1, x2)
+x3 = x1.mm(x2)
+
+# matrix exponentiation
+matrix_exp = torch.rand(5, 5)
+matrix_exp.matrix_power(3)
+
+# element wise multiplication
+z = x * y 
+print(z)
